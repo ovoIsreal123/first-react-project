6ef2b59176993f8ce14d663c1a2c6224e40b5016
AGBRO PC <you@example.com>
2024-07-17 00:20:15 +0100

    initial commit

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -7,21 +7,12 @@ export const Leftboxes = () => {
   return (
     <div className='leftbox'>
         <div className ='toptwo'>
-<<<<<<< HEAD
             <Box title={"GST101"}/>
             <Box  title={"COS01"}/>
         </div>
         <div className ='assignments'>
           <Box title = {"Advert"} />
           <Box title={"Assignments"} />
-=======
-            <Box title={"GST01"}/>
-            <Box  title={"COS1"}/>
-        </div>
-        <div className ='assignments'>
-          <Box title = {"Advert"} />
-          <Box title={"Assignment"} />
->>>>>>> 1f5d669d083b999bc0ab5a321637dc1e80db7f4c
         </div>
     </div>
   )
