1f5d669d083b999bc0ab5a321637dc1e80db7f4c
ovoIsreal123 <165371086+ovoIsreal123@users.noreply.github.com>
2024-07-16 21:52:55 +0100

    Update index.js

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -7,12 +7,12 @@ export const Leftboxes = () => {
   return (
     <div className='leftbox'>
         <div className ='toptwo'>
-            <Box title={"cos1"}/>
-            <Box  title={"cos1"}/>
+            <Box title={"GST01"}/>
+            <Box  title={"COS1"}/>
         </div>
         <div className ='assignments'>
-          <Box title = {"cos1"} />
-          <Box title={"cos1"} />
+          <Box title = {"Advert"} />
+          <Box title={"Assignment"} />
         </div>
     </div>
   )
