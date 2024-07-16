356fd07a8fabea06b8e420367507d6ec6e7d71ab
AGBRO PC <you@example.com>
2024-07-16 21:48:26 +0100

    Updates

---
+++ precache-manifest.1d5de269c89df5e9f633710b050e132f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "347c346b598e4060ed72319c4e8d78fe",
+    "url": "/first-react-project/index.html"
+  },
+  {
+    "revision": "8c209f9f5ffd93c6e8fd",
+    "url": "/first-react-project/static/css/main.079a7c51.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/first-react-project/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "8c209f9f5ffd93c6e8fd",
+    "url": "/first-react-project/static/js/main.c34dba8a.chunk.js"
+  },
+  {
+    "revision": "aaacb9a8b5a0d7ee6692",
+    "url": "/first-react-project/static/js/runtime~main.dbb18081.js"
+  }
+]);
